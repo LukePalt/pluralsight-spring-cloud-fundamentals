b6cf1b6ddc1923c89cdacb47391c65810e05c351
Łukasz Palt <lukasz.palt@gmail.com>
2018-02-05 19:54:56 +0100

    Add Ribbon service module.

---
+++ ribbon-time-service/src/main/java/io/schultz/dustin/ribbontimeservice/RibbonTimeServiceApplication.java
@@ -0,0 +1,12 @@
+package io.schultz.dustin.ribbontimeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonTimeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonTimeServiceApplication.class, args);
+	}
+}
