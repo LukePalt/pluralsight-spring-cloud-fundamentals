dd505476e384ccd7a77b59777915c0179a89f4db
LukePalt <lukasz.palt@gmail.com>
2018-02-05 20:40:44 +0100

    Complete Ribbon example.

--- ribbon-time-app/src/main/java/io/schultz/dustin/ribbontimeapp/RibbonTimeAppApplication.java
+++ ribbon-time-app/src/main/java/io/schultz/dustin/ribbontimeapp/RibbonTimeAppApplication.java
@@ -1,12 +1,37 @@
 package io.schultz.dustin.ribbontimeapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
+@EnableDiscoveryClient
+@Configuration
 public class RibbonTimeAppApplication {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RibbonTimeAppApplication.class, args);
 	}
+
+	@GetMapping
+	public String getCurrentTime() {
+	    return restTemplate.getForEntity("http://time-service", String.class).getBody();
+    }
+
+	@Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+	    return new RestTemplate();
+    }
 }
