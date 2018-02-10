53a00680f3340e4e901c2c6c28a2b463925e3fd8
LukePalt <lukasz.palt@gmail.com>
2018-02-10 13:04:26 +0100

    Custom Ribbon configuration.

--- ribbon-time-app/src/main/java/io/schultz/dustin/ribbontimeapp/RibbonTimeAppApplication.java
+++ ribbon-time-app/src/main/java/io/schultz/dustin/ribbontimeapp/RibbonTimeAppApplication.java
@@ -1,5 +1,6 @@
 package io.schultz.dustin.ribbontimeapp;
 
+import io.schultz.dustin.configuration.RibbonTimeConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +16,9 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
-@RibbonClient(name = "time-service")
+@RibbonClient(name = "time-service", configuration = RibbonTimeConfiguration.class)
 @Configuration
+@ComponentScan("io.schultz.dustin.ribbontimeapp")
 public class RibbonTimeAppApplication {
 
     @Autowired
